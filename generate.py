5c66c023b31e36ff0364635dd37284fc1c977229
SilicatYT <silicat.yt@gmx.ch>
2026-09-05 14:05:33 +0200

    Add group & branch creation
    - Renamed block_tag_data in gen_block_id_tags() to block_tags_data to avoid confusion
    - Reworked get_block_state_groups() to get_value_class_branches(). It now references groups instead of individual blocks
    - Added get_block_state_groups()
    - Added gen_block_state_group_tags()
    - Removed get_properties_with_mult_value_classes() due to being unused
    - Removed gen_property_value_class_tags() and added its behaviour to the new gen_value_class_tags() that also generates branch tags

--- generate.py
+++ generate.py
@@ -25,14 +25,14 @@ def get_block_property_data():
 
 
 def gen_block_id_tags(bit_count, blocks):
-    block_tag_data = [{"values": []} for _ in range(bit_count)] # Using EMPTY_BLOCK_TAG_CONTENT instead of {...} makes them all share a reference, so don't do that
+    block_tags_data = [{"values": []} for _ in range(bit_count)] # Using EMPTY_BLOCK_TAG_CONTENT instead of {...} makes them all share a reference, so don't do that
 
     # Fill block tag data
     for i, block_id in enumerate(blocks):
         bits = [(i >> bit) & 1 for bit in range(bit_count - 1, -1, -1)] # Create a list of bits that represent i, taken from stackoverflow
         for j, bit in enumerate(bits):
             if bit == 1:
-                block_tag_data[j]["values"].append(block_id)
+                block_tags_data[j]["values"].append(block_id)
 
     # Write data to block tags
     folder_path = BLOCK_TAGS_FOLDER_PATH / "block_id"
@@ -41,7 +41,7 @@ def gen_block_id_tags(bit_count, blocks):
     for i in range(bit_count):
         file_path = folder_path / f"b{i}.json"
         with file_path.open("w") as f:
-            json.dump(block_tag_data[i], f, separators=(',', ':'))
+            json.dump(block_tags_data[i], f, separators=(',', ':'))
 
 
 def gen_block_id_provider(bit_count):
@@ -54,65 +54,79 @@ def gen_block_id_provider(bit_count):
         json.dump(number_provider_data, f, separators=(',', ':'))
 
 
-def get_properties_with_mult_value_classes(block_property_data):
-    properties = {}
-    for property, value_classes in block_property_data.items():
-        count = len(value_classes)
-        if count >= 1:
-            properties[property] = count
-    return properties
-
-
-def gen_property_value_class_tags(block_value_data, properties): # For block state properties that have more than 1 value class, generates a block tag for every value class except the last one
-    for property, value_classes_count in properties.items():
-        for i in range(1, value_classes_count): # Skip the 1st instead of last one, because heuristically, earlier value classes have more block entries
-            ids = [block_id for block_id, states in block_value_data.items() if property in states and states[property]["value_class"] == i]
-            block_tag_data = {"values": ids}
-
-            folder_path = BLOCK_TAGS_FOLDER_PATH / "value_class" / property
-            folder_path.mkdir(parents=True, exist_ok=True)
-
-            file_path = folder_path / f"{i}.json"
-            with file_path.open("w") as f:
-                json.dump(block_tag_data, f, separators=(',', ':'))
-
-
-def get_block_state_groups(block_value_data): # A block state group is a collection of block ids with the same value class and the same available values for any given property
-    groups = {} # {"age":{<value_class>:{<tuple_of_possible_values>:[<blocks in that group>]}}, ...}
+def get_block_state_groups(block_value_data): # A group consists of blocks that share the same possible block states across all properties.
+    groups = {} # {(tuple containing all properties with value class & possible values):[all blocks that match]}
     for block, block_data in block_value_data.items():
-        for property, value_data in block_data.items():
-            if not property in groups:
-                groups[property] = {}
+        key = tuple(
+            (property, value_data["value_class"], tuple(value_data["values"]))
+            for property, value_data in sorted(block_data.items()) # Should already be pre-sorted from the datagen, but just in case
+        )
 
-            value_class = value_data["value_class"]
-            if not value_class in groups[property]:
-                groups[property][value_class] = {}
+        if not key in groups:
+            groups[key] = set()
 
-            possible_values = tuple(value_data["values"]) # Tuple because lists can't be keys
-            if not possible_values in groups[property][value_class]:
-                groups[property][value_class][possible_values] = []
-            groups[property][value_class][possible_values].append(block)
+        if not block in groups[key]:
+            groups[key].add(block)
 
-    for property, group_data in list(groups.items()): # Values inside the list are still by reference
-
-        # Prune value classes with only 1 group
-        for value_class in list(group_data): # list() because otherwise I'd be iterating over the live object. Deleting while iterating is a bad idea
-            if len(group_data[value_class]) == 1:
-                del group_data[value_class]
-
-        # Prune property if no value classes remain
-        if len(group_data) == 0:
-            del groups[property]
+    for key in groups:
+        groups[key] = sorted(groups[key]) # Sorted already converts to a list, no list() necessary
 
     return groups
 
 
-
 def gen_block_state_group_tags(groups):
-    pass
+    folder_path = BLOCK_TAGS_FOLDER_PATH / "groups"
+    folder_path.mkdir(parents=True, exist_ok=True)
+
+    for i, blocks in enumerate(groups.values()):
+        block_tag_data = {"values": blocks}
+
+        file_path = folder_path / f"{i}.json"
+        with file_path.open("w") as f:
+            json.dump(block_tag_data, f, separators=(',', ':'))
 
 
-def gen_individual_block_state_providers(block_property_data):
+
+def get_value_class_branches(groups): # Different branches of the same value class have different possible values, but pull from the same value pool.
+    branches = {} # {"age":{<value class>:{<tuple of possible values>:[<indices of groups in that branch>]}}, ...}
+    for i, block_state_data in enumerate(groups):
+        for property, value_class, values in block_state_data:
+            if not property in branches:
+                branches[property] = {}
+
+            if not value_class in branches[property]:
+                branches[property][value_class] = {}
+
+            if not values in branches[property][value_class]:
+                branches[property][value_class][values] = []
+
+            branches[property][value_class][values].append(i)
+
+    return branches
+
+
+def gen_value_class_tags(branches): # Block tag for each group within the value class, and a combined one for the total value class
+    for property, value_classes in branches.items():
+        for value_class_index, branches in value_classes.items():
+            folder_path = BLOCK_TAGS_FOLDER_PATH / "value_classes" / property / str(value_class_index)
+            folder_path.mkdir(parents=True, exist_ok=True)
+
+            value_class_block_tag_data = {"values": []}
+
+            for branch_index, groups in enumerate(branches.values()):
+                value_class_block_tag_data["values"].append(f"{DATAPACK_NAMESPACE}:value_classes/{property}/{value_class_index}/branch_{branch_index}.json")
+                branch_block_tag_data = {"values": [f"#{DATAPACK_NAMESPACE}:groups/{group}" for group in groups]}
+
+                file_path = folder_path / f"branch_{branch_index}.json"
+                with file_path.open("w") as f:
+                    json.dump(branch_block_tag_data, f, separators=(',', ':'))
+
+            file_path = folder_path / "all.json"
+            with file_path.open("w") as f:
+                json.dump(value_class_block_tag_data, f, separators=(',', ':'))
+
+
+def gen_individual_block_state_providers(block_property_data): # TODO: Rework so it distinguishes between the branches
     for property, value_classes in block_property_data.items():
         if len(value_classes) == 1:
             number_provider_data = get_individual_block_state_provider_body(property, value_classes[0], 0)
@@ -140,7 +154,7 @@ def gen_individual_block_state_providers(block_property_data):
             json.dump(number_provider_data, f, separators=(',', ':'))
 
 
-def get_individual_block_state_provider_body(property, possible_states, value_offset):
+def get_individual_block_state_provider_body(property, possible_states, value_offset): # TODO: Rework so it distinguishes between the branches
     # Base cases
     if len(possible_states) == 1:
         return value_offset
@@ -167,8 +181,8 @@ def get_individual_block_state_provider_body(property, possible_states, value_of
 
 # Logic:
 # - Inside a value class, name the block tags for "value range" 'branch'
-# - In the publicly available individual block state providers, first check the value class in a number dispatcher, and make it run another provider that checks the branch. OR FLATTEN THE TWO CHECKS INTO A SINGLE "block state group" CHECK? -> Don't flatten, because multiple block state groups could have the same values for this particular block state. That provider (inlined) calls the internal per-branch provider that uses binary search (basically the provider body).
-# - In the "get_block_state" (merged) provider, check the block state group at the very top (default to <none>), maybe binary search it if there are a lot of groups. Then inside, run the individual block state providers (the internal ones: At build-time, I already know which branch & value classes I'm on) and combine the results. Smallest to largest, or largest to smallest? Doesn't matter, I just need to be consistent. It's called "mixed-radix"
+# - In the publicly available individual block state providers, first check the value class in a number dispatcher, and make it run another provider that checks the branch. OR FLATTEN THE TWO CHECKS INTO A SINGLE "block state branch" CHECK? -> Don't flatten, because multiple block state branches could have the same values for this particular block state. That provider (inlined) calls the internal per-branch provider that uses binary search (basically the provider body).
+# - In the "get_block_state" (merged) provider, check the block state branch at the very top (default to <none>), maybe binary search it if there are a lot of branches. Then inside, run the individual block state providers (the internal ones: At build-time, I already know which branch & value classes I'm on) and combine the results. Smallest to largest, or largest to smallest? Doesn't matter, I just need to be consistent. It's called "mixed-radix"
 # - I can re-use the same "get_block_state (merged)" provider for the "single int for whole block id + blockstate" and for "int for blockid, int for blockstate". I just need to add a different prefix at the end
 
 # Run
@@ -183,7 +197,8 @@ gen_block_id_provider(bit_count)
 groups = get_block_state_groups(block_value_data)
 gen_block_state_group_tags(groups)
 
+branches = get_value_class_branches(groups)
+gen_value_class_tags(branches)
+
 block_property_data = get_block_property_data()
-properties = get_properties_with_mult_value_classes(block_property_data)
-gen_property_value_class_tags(block_value_data, properties)
 gen_individual_block_state_providers(block_property_data)
